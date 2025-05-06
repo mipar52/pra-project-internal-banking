bf09f12a613b094ee615e3b91493b1c074713ba6
Domagoj Antić <dantic1@algebra.hr>
2025-05-06 12:23:39 +0200

    [dantic] Refractor: Inherit from abstract Transaction class and kept custom propertie

--- PRA_1/Models/ParkingPayment.cs
+++ PRA_1/Models/ParkingPayment.cs
@@ -3,25 +3,11 @@ using System.Collections.Generic;
 
 namespace PRA_1.Models;
 
-public partial class ParkingPayment
+public partial class ParkingPayment : Transaction
 {
-    public int Id { get; set; }
-
-    public int UserId { get; set; }
-
     public string RegistrationCountryCode { get; set; } = null!;
-
     public string RegistrationNumber { get; set; } = null!;
-
-    public decimal Amount { get; set; }
-
     public int DurationHours { get; set; }
-
     public DateTime StartTime { get; set; }
-
     public DateTime? EndTime { get; set; }
-
-    public DateTime PaymentDate { get; set; }
-
-    public virtual User User { get; set; } = null!;
 }
