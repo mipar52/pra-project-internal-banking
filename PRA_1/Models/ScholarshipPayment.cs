09fe2a4677083cb695ca456683128550a79ef4f2
Domagoj Antić <dantic1@algebra.hr>
2025-05-06 12:48:51 +0200

    [dantic] Refractor: Inherit from abstract Transaction class and kept specific properties

--- PRA_1/Models/ScholarshipPayment.cs
+++ PRA_1/Models/ScholarshipPayment.cs
@@ -3,25 +3,11 @@ using System.Collections.Generic;
 
 namespace PRA_1.Models;
 
-public partial class ScholarshipPayment
+public partial class ScholarshipPayment : Transaction
 {
-    public int IdScholarshipPayment { get; set; }
-
-    public int UserId { get; set; }
-
     public int StudyProgramId { get; set; }
-
     public string? PaymentPlan { get; set; }
-
     public int? InstallmentNumber { get; set; }
-
     public int? TotalInstallments { get; set; }
-
-    public decimal Amount { get; set; }
-
-    public DateTime PaymentDate { get; set; }
-
     public virtual StudyProgram StudyProgram { get; set; } = null!;
-
-    public virtual User User { get; set; } = null!;
 }
