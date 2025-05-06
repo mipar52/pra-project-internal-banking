1eaebe35395917e814e4f5d15df020742bae910b
Domagoj Antić <dantic1@algebra.hr>
2025-05-06 13:16:47 +0200

    [dantic] Add WalletTopUpTransaction class, inheriting from abstract Transaction class

---
+++ PRA_1/Models/WalletTopUpTransaction.cs
@@ -0,0 +1,8 @@
+﻿namespace PRA_1.Models
+{
+    public class WalletTopUpTransaction : Transaction
+    {
+        public int CreditCardId { get; set; }
+        public virtual CreditCard CreditCard { get; set; } = null; 
+    }
+}
