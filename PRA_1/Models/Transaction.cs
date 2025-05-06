c07635888c5d3cfbd209cdd8877d1474948e556f
Domagoj Antić <dantic1@algebra.hr>
2025-05-06 11:59:26 +0200

    Add Transaction class
    Parent class for all transactions

---
+++ PRA_1/Models/Transaction.cs
@@ -0,0 +1,13 @@
+﻿namespace PRA_1.Models
+{
+    public abstract class Transaction
+    {
+        public int IdTransaction { get; set; }
+        public int UserId { get; set; } //FK na usera
+        public int TransactionTypeId{ get; set; } //FK na TransType
+        public decimal Amount { get; set; }
+        public DateTime TransactionDate { get; set; }
+        
+        public virtual User User { get; set; }
+        public virtual TransactionType TransactionType { get; set; }
+}
