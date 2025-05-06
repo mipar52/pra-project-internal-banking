a5901c7594a8ec4dc67afec357cc2b638faffd30
Domagoj Antić <dantic1@algebra.hr>
2025-05-06 13:00:04 +0200

    [dantic] Refractor: Change class name, inherit from abstract Transaction class and keep custom properties

---
+++ PRA_1/Models/MoneyTransfer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PRA_1.Models;
+
+public partial class SendRequestMoney : Transaction
+{
+    public int UserRecieverId { get; set; } // UserSender je onaj User u Transaction klasi
+    public virtual User UserReciever { get; set; } = null!;
+}
